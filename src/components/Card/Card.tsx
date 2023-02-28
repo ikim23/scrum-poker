f91c04dc9924a6833d7cc06ff352164b86845fc4
ikim23 <ikim23@users.noreply.github.com>
2023-02-28 13:44:57 +0100

    room

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,14 @@
+import { Button } from "flowbite-react";
+
+type CardProps = {
+  value: string;
+  onClick: () => void;
+};
+
+export function Card({ value, onClick }: CardProps) {
+  return (
+    <Button className="h-[8rem] w-[8rem]" onClick={onClick}>
+      <span className=" text-5xl">{value}</span>
+    </Button>
+  );
+}
--- src/pages/rooms/[roomId].tsx
+++ src/pages/rooms/[roomId].tsx
@@ -1,5 +1,26 @@
+import { Card } from "~/components/Card/Card";
 import { Layout } from "~/components/Layout/Layout";
 
+const VALUES = ["1", "2", "3", "5", "8", "13", "20", "40", "100"];
+
 export default function Room() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <div className="flex">
+        <div className="inline-grid grid-cols-3 gap-4">
+          {VALUES.map((value) => (
+            <Card key={value} value={value} onClick={() => {}} />
+          ))}
+        </div>
+        <div className="ml-auto">
+          <h2 className="mb-4 text-3xl">Connected Users</h2>
+          <ul>
+            <li>John</li>
+            <li>Pete</li>
+            <li>Becky</li>
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  );
 }
