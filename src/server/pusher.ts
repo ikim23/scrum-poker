b0df8bdabb1eb4e71959bb70c329586a8298c48e
ikim23 <ikim23@users.noreply.github.com>
2023-02-28 16:21:29 +0100

    pusher global

--- src/server/pusher.ts
+++ src/server/pusher.ts
@@ -1,10 +1,16 @@
 import Pusher from "pusher";
 import { env } from "~/env.mjs";
 
-export const pusher = new Pusher({
-  appId: env.PUSHER_APP_ID,
-  key: env.NEXT_PUBLIC_PUSHER_KEY,
-  secret: env.PUSHER_SECRET,
-  cluster: env.NEXT_PUBLIC_PUSHER_CLUSTER,
-  useTLS: true,
-});
+const globalForPusher = globalThis as unknown as { pusher: Pusher };
+
+export const pusher =
+  globalForPusher.pusher ||
+  new Pusher({
+    appId: env.PUSHER_APP_ID,
+    key: env.NEXT_PUBLIC_PUSHER_KEY,
+    secret: env.PUSHER_SECRET,
+    cluster: env.NEXT_PUBLIC_PUSHER_CLUSTER,
+    useTLS: true,
+  });
+
+if (env.NODE_ENV !== "production") globalForPusher.pusher = pusher;
