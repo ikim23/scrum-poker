c7b4c2394714194b5f7361db6952aeff04754bf8
ikim23 <ikim23@users.noreply.github.com>
2023-05-26 16:44:06 +0200

    add counter.dev

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,7 @@
 import { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
+import { env } from '~/env.mjs'
 
 export default function Document() {
   return (
@@ -7,6 +10,14 @@ export default function Document() {
       <body className="dark text-white">
         <Main />
         <NextScript />
+        {env.NODE_ENV === 'production' && (
+          <Script
+            data-id="30cbe9bf-51ff-4725-92cd-33f83c4640bb"
+            data-utcoffset="2"
+            src="https://cdn.counter.dev/script.js"
+            strategy="lazyOnload"
+          />
+        )}
       </body>
     </Html>
   )
