e366c7d754cb09f3cb2c9ced19123ae585c40faa
ikim23 <ikim23@users.noreply.github.com>
2023-05-30 16:31:55 +0200

    delete room

--- src/components/RoomList/RoomList.tsx
+++ src/components/RoomList/RoomList.tsx
@@ -1,14 +1,26 @@
 import Link from 'next/link'
-import { FiExternalLink } from 'react-icons/fi'
+import { FiExternalLink, FiTrash2 } from 'react-icons/fi'
 
 import { Routes } from '~/routes'
-import { type RouterOutputs } from '~/utils/trpc'
+import { type RouterOutputs, trpc } from '~/utils/trpc'
 
 type RoomListProps = {
   rooms: RouterOutputs['room']['getRooms']
 }
 
 export function RoomList({ rooms }: RoomListProps) {
+  const trpcContext = trpc.useContext()
+  const { mutate: deleteRoom } = trpc.room.deleteRoom.useMutation({
+    onMutate({ roomId }) {
+      trpcContext.room.getRooms.setData(undefined, (prevRooms) =>
+        prevRooms ? prevRooms.filter((room) => room.roomId !== roomId) : undefined
+      )
+    },
+    onSuccess() {
+      void trpcContext.room.getRooms.invalidate()
+    },
+  })
+
   if (rooms.length === 0) {
     return <div>No rooms yet!</div>
   }
@@ -16,13 +28,22 @@ export function RoomList({ rooms }: RoomListProps) {
   return (
     <ul className="list-inside list-disc">
       {rooms.map((room) => (
-        <li className="my-2" key={room.roomId}>
-          <Link className="inline-block" href={Routes.room(room.roomId)}>
-            <div className="flex items-center gap-1">
+        <li className="my-2 text-lg" key={room.roomId}>
+          <div className="group inline-flex items-center gap-2">
+            <Link className="flex items-center gap-[inherit] hover:underline" href={Routes.room(room.roomId)}>
               <span>{room.name}</span>
-              <FiExternalLink />
-            </div>
-          </Link>
+              <FiExternalLink className="inline-block" />
+            </Link>
+            <button
+              className="invisible rounded p-1 hover:bg-red-500 group-hover:visible"
+              onClick={() => {
+                deleteRoom({ roomId: room.roomId })
+              }}
+              type="button"
+            >
+              <FiTrash2 />
+            </button>
+          </div>
         </li>
       ))}
     </ul>
